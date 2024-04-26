6fbb0e08c666d7447b1ce77592b810794fc2d274
김은서 <94689360+KES123450@users.noreply.github.com>
2024-04-26 10:52:06 +0900

    Test : SMS 문자 수신 테스트 코드 작성

--- app/src/main/java/com/capstone/nongchown/View/Activity/MainActivity.kt
+++ app/src/main/java/com/capstone/nongchown/View/Activity/MainActivity.kt
@@ -6,10 +6,12 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.os.Build
-
 import android.os.Bundle
+import android.telephony.SmsManager
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -18,11 +20,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.capstone.nongchown.Model.ForegroundService
 import com.capstone.nongchown.R
-
 import com.capstone.nongchown.Utils.moveActivity
 
 class MainActivity() : AppCompatActivity() {
     private lateinit var sharedPreferences: SharedPreferences
+    private val PERMISSIONS_REQUEST_SEND_SMS = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -90,6 +92,35 @@ class MainActivity() : AppCompatActivity() {
 
         }
 
+        val smsBtn : Button = findViewById(R.id.btnSMS)
+        smsBtn.setOnClickListener{
+            val phoneText: EditText = findViewById(R.id.phone_number)
 
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.SEND_SMS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {//권한이 없다면
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.SEND_SMS),
+                    PERMISSIONS_REQUEST_SEND_SMS
+                )
+            } else { //권한이 있다면 SMS를 보낸다.
+
+                val smsManager = SmsManager.getDefault()
+                try {
+                    smsManager.sendTextMessage("+82"+phoneText.text.toString(), null, "안녕~~~", null,null)
+                } catch (ex: Exception) {
+                    ex.printStackTrace()
+                    Toast.makeText(baseContext, ex.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+
+
+        }
     }
+
+
 }
+
