41b6f0e0ad8b966f58cc9b6264c2d0232f7f2000
ssangttuce <sanghoo1023@gmail.com>
2024-04-26 10:16:07 +0900

    feat:add ids in each userprofile elements

--- app/src/main/java/com/capstone/nongchown/View/Activity/UserProfileActivity.kt
+++ app/src/main/java/com/capstone/nongchown/View/Activity/UserProfileActivity.kt
@@ -15,6 +15,9 @@ class UserProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_user_profile)
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.user_profile)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
