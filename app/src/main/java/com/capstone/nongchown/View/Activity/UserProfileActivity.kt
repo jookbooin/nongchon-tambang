2d286d7eff3bded8f7a0e83d905974bc09d0c74a
ssangttuce <sanghoo1023@gmail.com>
2024-05-01 23:35:22 +0900

    feat: userprofile variables initialize

--- app/src/main/java/com/capstone/nongchown/View/Activity/UserProfileActivity.kt
+++ app/src/main/java/com/capstone/nongchown/View/Activity/UserProfileActivity.kt
@@ -3,12 +3,15 @@ package com.capstone.nongchown.View.Activity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.capstone.nongchown.R
 import com.capstone.nongchown.Utils.moveActivity
+import com.capstone.nongchown.ViewModel.UserProfileViewModel
 
 class UserProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +19,20 @@ class UserProfileActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_user_profile)
 
+        val userName = findViewById<EditText>(R.id.user_name)
+        val userEmail = findViewById<EditText>(R.id.user_email)
+        val userPhone = findViewById<EditText>(R.id.user_phone)
+        val emergencyContact = findViewById<EditText>(R.id.emergency_contact)
+        val saveButton = findViewById<Button>(R.id.user_profile_saveButton)
+
+        saveButton.setOnClickListener {
+            val name = userName.text.toString()
+            val email = userEmail.text.toString()
+            val phone = userPhone.text.toString()
+            val eContact = emergencyContact.text.toString()
+
+            UserProfileViewModel().userProfileSave(name, email, phone, eContact)
+        }
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.user_profile)) { v, insets ->
---
+++ app/src/main/java/com/capstone/nongchown/ViewModel/UserProfileViewModel.kt
@@ -0,0 +1,14 @@
+package com.capstone.nongchown.ViewModel
+
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.widget.TextView
+import androidx.lifecycle.ViewModel
+import com.capstone.nongchown.R
+
+class UserProfileViewModel : ViewModel() {
+    fun userProfileSave(name: String, email: String, phone: String, emergencyContact: String) {
+
+
+    }
+
+}
